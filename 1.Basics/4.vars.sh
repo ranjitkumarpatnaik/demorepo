78651e248a1a700607a0297328ed35d171135c5f
ranjitkumarpatnaik <mr.ranjitpatnaik@gmail.com>
2022-01-08 12:35:35 +0530

    added few more variable examples

--- 1.Basics/4.vars.sh
+++ 1.Basics/4.vars.sh
@@ -10,3 +10,19 @@ echo "My surname is $b"
 echo
 echo "My class is $c"
 echo
+
+
+x=4
+y=5
+z=$[ $x+$y ]
+echo "Sum of x and y is  $z"
+echo
+
+#Taking command output and storing it in a variable
+today=$(date)
+echo "Today is $today"
+
+#Advanced echo options
+
+echo
+echo "The current logged in user is: $(whoami)"
